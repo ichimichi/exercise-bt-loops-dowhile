4323a0292b49bbfeaef6c7f9de43846a3f2e8540
Nishanth P <nishanth.p@stackroute.in>
2020-01-10 15:13:17 +0530

    pom updated

--- src/main/java/com/stackroute/basics/Palindrome.java
+++ src/main/java/com/stackroute/basics/Palindrome.java
@@ -14,16 +14,17 @@ public class Palindrome {
 
     }
 
+    // validate the input using inputValidator and print the error message or the result from palindromeValidator
     public void outputPrinter(String number) {
 
     }
-
+    // check whether the input is a valid integer and return boolean
     public boolean inputValidator(String number) {
         return true;
     }
 
 
-    //write logic to check for palindrome using BigInteger & Do-while
+    // write logic to check for palindrome using BigInteger & Do-while
     public String palindromeValidator(BigInteger number) {
         return null;
     }
